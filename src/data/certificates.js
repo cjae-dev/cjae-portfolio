78e6f11b5ca661ad9bac6a487ce83e01d9384d26
cjae-dev <cjaelimos04@gmail.com>
2026-05-19 20:59:55 +0800

    update certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -4,5 +4,11 @@ export const certificates = [
     issuer: 'Udemy - Maximilian Schwarzmüller',
     description: 'Teaches the latest version of Next.js including App Router and Pages Router.',
     image: `${import.meta.env.BASE_URL}images/certificates/cjae-nextjs-react-certificate.jpg`
+  },
+  {
+    title: 'Digital Marketing Expert Program',
+    issuer: 'NeoDocto',
+    description: 'Included on Internship program for Website Development with NeoDocto Inc.',
+    image: `${import.meta.env.BASE_URL}images/certificates/cjae-digital-marketing-certificate.jpg`
   }
 ];
