f7535e587bc0f397b797d763292a71bbb3f4ef1f
C jae Limos <cjaelimos04@gmail.com>
2026-07-20 18:41:30 +0800

    add laravel certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -18,6 +18,12 @@ export const certificates = [
     image: `${import.meta.env.BASE_URL}images/certificates/cjae-drupal-11-certificate.jpg`
   },
   {
+    title: 'Laravel & PHP Mastery: Build 5 Real-World Projects',
+    issuer: 'Udemy - Piotr Jura',
+    description: 'Master Laravel, PHP & full-stack skills by building 5 production-ready apps!',
+    image: `${import.meta.env.BASE_URL}images/certificates/cjae-laravel-5-php-certificate.jpg`
+  },
+  {
     title: 'Digital Marketing Expert Program',
     issuer: 'NeoDocto',
     description: 'Included on Internship program for Website Development with NeoDocto Inc.',
